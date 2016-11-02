99f745bfce89d134dc95d511364d6ae4e04b2b80
Brian Cohen <brian.cohen0356@gmail.com>
2016-11-02 10:33:51 -0500

    Add completed and created timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -10,29 +10,45 @@ describe('TodoApp', () => {
         expect(TodoApp).toExist();
     });
     it('should add todo to the todos state on handleAddTodo', () => {
-       var todoText = 'test text';
-       var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+        var todoText = 'test text';
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
 
-       todoApp.setState({todos: []});
-       todoApp.handleAddTodo(todoText);
-
-       expect(todoApp.state.todos[0].text).toBe(todoText);
+        todoApp.setState({todos: []});
+        todoApp.handleAddTodo(todoText);
+        expect(todoApp.state.todos[0].text).toBe(todoText);
+        expect(todoApp.state.todos[0].createdAt).toBeA('number');
     });
     it('should toggle completed value when handleToggle is called', () => {
-       var todoData = {
-           id: 11,
-           text: 'Test features',
-           completed: false,
-       }
-       var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+        var todoData = {
+            id: 11,
+            text: 'Test features',
+            completed: false,
+            createdAt: 0,
+            completedAt: undefined,
+        };
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
 
-       todoApp.setState({todos: [todoData]});
+        todoApp.setState({todos: [todoData]});
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        todoApp.handleToggle(11);
+        expect(todoApp.state.todos[0].completed).toBe(true);
+        expect(todoApp.state.todos[0].completedAt).toBeA('number');
 
-       expect(todoApp.state.todos[0].completed).toBe(false);
-       todoApp.handleToggle(11);
+    });
+    it('should toggle todo from completed to not completed', () => {
+        var todoData = {
+            id: 12,
+            text: 'Test features',
+            completed: true,
+            createdAt: 0,
+            completedAt: 1478034506,
+        };
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
 
-       expect(todoApp.state.todos[0].id).toBe(11);
-       expect(todoApp.state.todos[0].completed).toBe(true);
-
+        todoApp.setState({todos: [todoData]});
+        expect(todoApp.state.todos[0].completed).toBe(true);
+        todoApp.handleToggle(12);
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        expect(todoApp.state.todos[0].completedAt).toNotExist();
     });
 });
